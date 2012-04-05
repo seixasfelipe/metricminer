6cb08fc00e8a2ab9ba7d7333c56b812e1154055e
Francisco Sokol <chico.sokol@gmail.com>
2012-04-05 17:09:33 -0300

    adding dropall.sql to drop all tables if needed

---
+++ src/main/resources/dropall.sql
@@ -0,0 +1,15 @@
+SET foreign_key_checks = 0;
+
+
+DROP TABLE  `Artifact` ,
+`Author` ,
+`Commit` ,
+`Commit_Artifact` ,
+`ConfigurationEntry` ,
+`Modification` ,
+`Project` ,
+`SourceCode` ,
+`Task` ,
+`Task_Task` ;
+
+SET foreign_key_checks = 1;
