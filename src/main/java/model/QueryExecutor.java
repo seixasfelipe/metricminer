508c4bc805130063ec7279383239e0afb89e9331
Francisco Sokol <chico.sokol@gmail.com>
2012-04-26 17:13:40 -0300

    printing column names

--- src/main/java/model/QueryExecutor.java
+++ src/main/java/model/QueryExecutor.java
@@ -29,12 +29,21 @@ public class QueryExecutor {
 
     private void writeCSVTo(OutputStream csvOutputStream, List<Map<String,Object>> results) {
         PrintStream csvPrint = new PrintStream(csvOutputStream);
+        Map<String, Object> first = results.get(0);
+        printHeader(csvPrint, first);
         for (Map<String,Object> row : results) {
             for (Entry<String, Object> entry : row.entrySet()) {
                 csvPrint.print(entry.getValue() + ";");
             }
             csvPrint.print("\n");
         }
+    }
+
+    private void printHeader(PrintStream csvPrint, Map<String, Object> first) {
+        for (Entry<String, Object> entry : first.entrySet()) {
+            csvPrint.print(entry.getKey() + ";");
+        }
+        csvPrint.print("\n");
     }
 
 }
