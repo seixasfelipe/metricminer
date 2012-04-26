67958d31b1e488c7c694055a6f5da46eb4b288dd
Francisco Sokol <chico.sokol@gmail.com>
2012-04-26 14:20:31 -0300

    paginating results while calculating metrics

--- src/main/java/tasks/CalculateMetricTask.java
+++ src/main/java/tasks/CalculateMetricTask.java
@@ -2,6 +2,7 @@ package tasks;
 
 import java.io.ByteArrayInputStream;
 import java.util.Collection;
+import java.util.List;
 
 import model.Project;
 import model.SourceCode;
@@ -9,8 +10,6 @@ import model.Task;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
-import org.hibernate.ScrollMode;
-import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.hibernate.StatelessSession;
 
@@ -25,6 +24,8 @@ public class CalculateMetricTask implements RunnableTask {
     private Session session;
     private static Logger log = Logger.getLogger(CalculateMetricTask.class);
     private final StatelessSession statelessSession;
+    private int pageSize;
+    private Project project;
 
     public CalculateMetricTask(Task task, Metric metric, Session session,
             StatelessSession statelessSession) {
@@ -36,16 +37,26 @@ public class CalculateMetricTask implements RunnableTask {
 
     @Override
     public void run() {
-        Project project = task.getProject();
-        Query query = statelessSession.createQuery("select source from SourceCode source "
+        pageSize = 100;
+        int page = 0;
+        project = task.getProject();
+        List<SourceCode> sources = listSources(page);
+        while (!sources.isEmpty()) {
+            for (SourceCode sourceCode : sources) {
+                calculateAndSaveResultsOf(sourceCode);
+            }
+            page++;
+        }
+    }
+
+    private List<SourceCode> listSources(int page) {
+        Query query = session.createQuery("select source from SourceCode source "
                 + "join source.artifact as artifact where artifact.project.id = :project_id");
         query.setParameter("project_id", project.getId());
-
-        ScrollableResults sources = query.scroll(ScrollMode.FORWARD_ONLY);
-        while (sources.next()) {
-            SourceCode source = (SourceCode) sources.get(0);
-            calculateAndSaveResultsOf(source);
-        }
+        query.setFirstResult(page * pageSize);
+        query.setMaxResults(page * pageSize + pageSize);
+        List<SourceCode> sources = query.list();
+        return sources;
     }
 
     private void calculateAndSaveResultsOf(SourceCode sourceCode) {
