f9fe77524cc26825aeb9efdd6ead3de40a7c2d41
Francisco Sokol <chico.sokol@gmail.com>
2012-04-26 15:59:00 -0300

    creating query controller and refactoring query executor

--- src/main/java/model/Query.java
+++ src/main/java/model/Query.java
@@ -3,11 +3,18 @@ package model;
 public class Query {
     private String sql;
 
+    public Query() {
+    }
+    
     public Query(String query) {
         this.sql = query;
     }
 
     public String getSql() {
         return sql;
+    }
+    
+    public void setSql(String sql) {
+        this.sql = sql;
     }
 }
