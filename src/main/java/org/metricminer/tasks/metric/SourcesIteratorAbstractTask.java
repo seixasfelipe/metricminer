b4da7d23ae4032f9c7445575f29620fe49969556
Mauricio Aniche <mauricioaniche@gmail.com>
2012-06-19 16:13:25 -0300

    pequenas refatoracoes

--- src/main/java/org/metricminer/tasks/metric/SourcesIteratorAbstractTask.java
+++ src/main/java/org/metricminer/tasks/metric/SourcesIteratorAbstractTask.java
@@ -15,6 +15,7 @@ import org.metricminer.tasks.RunnableTask;
 
 public abstract class SourcesIteratorAbstractTask implements RunnableTask {
 
+	private static final int PAGE_SIZE = 5;
 	protected Task task;
 	protected Session session;
 	protected static Logger log = Logger.getLogger(SourcesIteratorAbstractTask.class);
@@ -32,25 +33,21 @@ public abstract class SourcesIteratorAbstractTask implements RunnableTask {
 
 		log.debug("Starting to iterate over sources");
 
-		Map<Long, String> map = sourceCodeDAO.listSourceCodeIdsAndNamesFor(project);
-		List<Long> sourceIds = new ArrayList<Long>(map.keySet());
+		Map<Long, String> idsAndNames = sourceCodeDAO.listSourceCodeIdsAndNamesFor(project);
+		List<Long> sourceIds = new ArrayList<Long>(idsAndNames.keySet());
 
 		int totalIds = sourceIds.size();
 
-		int PAGE_SIZE = 5;
 		for (int i = 0; i < totalIds; i += PAGE_SIZE) {
 			Long firstId = sourceIds.get(i);
-			Long lastId;
-			int lastIdIndex = i + (PAGE_SIZE - 1);
-			if (lastIdIndex < sourceIds.size())
-				lastId = sourceIds.get(lastIdIndex);
-			else
-				lastId = sourceIds.get(sourceIds.size() - 1);
+			Long lastId = calculateLastId(sourceIds, i);
+			
 			log.debug("Getting source codes (page " + i / PAGE_SIZE + ")");
+			
 			List<SourceCode> sources = sourceCodeDAO.listSourcesOf(project, firstId, lastId);
 			for (SourceCode sc : sources) {
-				log.debug("-- Working on " + map.get(sc.getId()) + " id " + sc.getId());
-				manipulate(sc, map.get(sc.getId()));
+				log.debug("-- Working on " + idsAndNames.get(sc.getId()) + " id " + sc.getId());
+				manipulate(sc, idsAndNames.get(sc.getId()));
 			}
 			System.gc();
 		}
@@ -58,6 +55,16 @@ public abstract class SourcesIteratorAbstractTask implements RunnableTask {
 		onComplete();
 		log.debug("Finished iterating over sources");
 
+	}
+
+	private Long calculateLastId(List<Long> sourceIds, int i) {
+		Long lastId;
+		int lastIdIndex = i + (PAGE_SIZE - 1);
+		if (lastIdIndex < sourceIds.size())
+			lastId = sourceIds.get(lastIdIndex);
+		else
+			lastId = sourceIds.get(sourceIds.size() - 1);
+		return lastId;
 	}
 
 	protected abstract void manipulate(SourceCode sourceCode, String name);
